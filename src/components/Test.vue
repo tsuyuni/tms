16d78c7a8262e854a7db1b6559653a17d9ecbb98
梅雨 <yuri.tsuchikawa@gmail.com>
2023-04-28 18:22:11 +0900

    ルーティングを実装

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+	title: string;
+	content: number;
+}
+defineProps<Props>();
+</script>
+
+<template>
+	<section>
+		<h1>{{title}}</h1>
+		<p>{{content}}</p>
+	</section>
+</template>
+
+<style scoped>
+section {
+	border: 1px solid red;
+	width: 500px;
+	height: 300px;
+}
+
+h1 {
+/*	color: red;*/
+}
+</style>
